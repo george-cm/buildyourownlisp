2516e77ae3c085c5063f470b8dbfb70569e3f6b8
George Murga <george.murga@gmail.com>
2022-10-15 19:44:00 +0300

    Add editline functionality

--- prompt.c
+++ prompt.c
@@ -1,26 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-/* Declare a buffer for user input of size 2048 */
-static char input[2048];
+#include <editline/readline.h>
+#include <editline/history.h>
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv)
+{
 
     /* Print Version and Exit Information */
     puts("Lispy Version 0.0.0.0.1");
     puts("Press Ctrl+c to Exit\n");
 
     /* In a never ending loop */
-    while (1) {
+    while (1)
+    {
 
         /* Output our prompt */
-        fputs("lispy> ", stdout);
+        char *input = readline("lispy> ");
 
-        /* Read a line of user input of maximum size 2048 */
-        fgets(input, 2048, stdin);
+        /* Add input to hisory */
+        add_history(input);
 
         /* Echo input back to user */
-        printf("No you're %s", input);
+        printf("No you're a %s\n", input);
 
+        /* Free retrieved input */
+        free(input);
     }
 
     return 0;
